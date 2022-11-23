028ff72c192255c81d8a634f3aecbd3c9f986fee
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2022-11-23 21:09:00 +0300

    added writing.c

---
+++ code/writing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+extern void validation(double n);
+
+void writing() {
+    FILE *file;
+    file = fopen("input.txt", "w");
+    double a = rand();
+    fprintf(file,"%lf", a);
+    fclose(file);
+    printf("%s\n", "Generated number is...");
+    printf("%lf\n", a);
+    validation(a);
+}
