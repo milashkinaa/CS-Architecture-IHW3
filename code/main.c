5771e579d373a19006d32ace6c5623d51caec503
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2022-11-25 01:14:32 +0300

    added main.c

--- code/main.c
+++ code/main.c
@@ -1,59 +1,27 @@
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/time.h>
-
-struct timeval tv1,tv2,dtv;
-struct timezone tz;
-void time_start() { gettimeofday(&tv1, &tz); }
-long time_stop()
-{ gettimeofday(&tv2, &tz);
-  dtv.tv_sec= tv2.tv_sec -tv1.tv_sec;
-  dtv.tv_usec=tv2.tv_usec-tv1.tv_usec;
-  if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
-  return dtv.tv_sec*1000+dtv.tv_usec/1000;
-}
-
-extern void sort(char* str);
-extern void delete_repeats(char* str);
-extern int equals(char* a, char elem);
-extern void common_elements(char* a, char* b);
-extern void writing();
+#include <math.h>
+ 
 extern void files();
+extern void writing();
 extern void console();
 
 int main(int argc, char** argv) {
-    FILE *file1, *file2;
-    file1 = fopen("input1.txt", "r");
-    file2 = fopen("input2.txt", "r");
-
-    time_start();
-
-    char* arg[1];
-    scanf("%d", arg);
-    if (arg[0] == 1) {
-            writing();
-        } else if (arg[0] == 2) {
+    if (argc == 2) {
+        printf("%s\n", "If you want to scan a value from a file, press 1");
+        printf("%s\n", "If you want to work with a random number, press 2");
+        printf("%s\n", "If you want to enter a value from the console, press 3");
+        int a;
+        scanf("%d", &a);
+        if (a == 1) {
             files();
-        } else if (arg[0] == 3) {
+        } else if (a == 2) {
+            writing();
+        } else if (a == 3) {
             console();
+        } else {
+            printf("%s\n", "You have entered an invalid value. Restart the program.");
         }
-    else {
-        return 0;
+    } else {                 
+        return 1;
     }
-
-    char str1[10000];
-    char str2[10000];
-    fgets(str1, 10000, file1);
-    fgets(str2, 10000, file2);
-    sort(str1);
-    delete_repeats(str1);
-    sort(str2);
-    delete_repeats(str2);
-    common_elements(str1, str2);
-    
-    printf("\nTime: %ld \n", time_stop());
-
-    return 0;
 }
