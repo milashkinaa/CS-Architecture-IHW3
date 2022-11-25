0938ef90a6387c81f598cd111ec7d8d4ce8984fd
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2022-11-25 22:21:55 +0500

    updated validation.c

--- assembly/validation.s
+++ assembly/validation.s
@@ -1,5 +1,5 @@
-	.file	"validation.c"
-	.intel_syntax noprefix
+	.file	"validation.c"                               # validation.c
+	.intel_syntax noprefix                               # используем синтаксис интел
 	.text
 	.section	.rodata
 .LC1:
@@ -11,27 +11,27 @@
 	.globl	validation
 	.type	validation, @function
 validation:
-	endbr64
+	endbr64                                              # void validation(double number) {
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 16
-	movsd	QWORD PTR -8[rbp], xmm0
+	movsd	QWORD PTR -8[rbp], xmm0                      # if (number <= 0.0000000001) {
 	movsd	xmm0, QWORD PTR .LC0[rip]
 	comisd	xmm0, QWORD PTR -8[rbp]
-	jb	.L6
-	lea	rax, .LC1[rip]
+	jb	.L6                                          # .L6
+	lea	rax, .LC1[rip]                               # printf("%s\n", "Incorrect data. Try again.");
 	mov	rdi, rax
-	call	puts@PLT
-	jmp	.L7
+	call	puts@PLT                                     # putc();
+	jmp	.L7                                          # .L7
 .L6:
-	lea	rax, .LC2[rip]
-	mov	rdi, rax
-	call	puts@PLT
-	mov	rax, QWORD PTR -8[rbp]
+	lea	rax, .LC2[rip]                               # } else {
+	mov	rdi, rax                                     # printf("%s\n", "Correct data. Your answer is...");
+	call	puts@PLT                                     # putc();
+	mov	rax, QWORD PTR -8[rbp]                       # calculation(number);
 	movq	xmm0, rax
-	call	calculation@PLT
+	call	calculation@PLT                              # calculation();
 .L7:
 	nop
 	leave
-	ret
+	ret                                                  # return();
 	.size	validation, .-validation
