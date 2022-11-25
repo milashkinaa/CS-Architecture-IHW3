905e43c213b02ec102d6c50aaad281fccf5556c6
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2022-11-25 21:55:45 +0500

    updated formula.s

--- assembly/formula.s
+++ assembly/formula.s
@@ -1,22 +1,22 @@
-	.file	"formula.c"
-	.intel_syntax noprefix
+	.file	"formula.c"                      # formula.c
+	.intel_syntax noprefix                   # используем синтаксис интел
 	.text
 	.globl	formula
 	.type	formula, @function
 formula:
-	endbr64
+	endbr64                                  # double formula(double n, double a) {
 	push	rbp
 	mov	rbp, rsp
 	movsd	QWORD PTR -8[rbp], xmm0
-	movsd	QWORD PTR -16[rbp], xmm1
-	movsd	xmm0, QWORD PTR -16[rbp]
+	movsd	QWORD PTR -16[rbp], xmm1         # return (0.5)*(n+a/n);
+	movsd	xmm0, QWORD PTR -16[rbp]         # /
 	divsd	xmm0, QWORD PTR -8[rbp]
-	movapd	xmm1, xmm0
+	movapd	xmm1, xmm0                       # +
 	addsd	xmm1, QWORD PTR -8[rbp]
-	movsd	xmm0, QWORD PTR .LC0[rip]
+	movsd	xmm0, QWORD PTR .LC0[rip]        # *
 	mulsd	xmm0, xmm1
 	movq	rax, xmm0
-	movq	xmm0, rax
+	movq	xmm0, rax                        # }
 	pop	rbp
-	ret
+	ret                                      # return();
 	.size	formula, .-formula
